f68ac04d81a7aef6fd57d7d764f8d369222b3737
anna soloveva <anyuta-angel22@yandex.ru>
2022-08-23 00:52:07 +0300

    added tatle new doc

--- Program.cs
+++ Program.cs
@@ -1,1 +1,74 @@
-﻿Задача 1 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+﻿//Задача 1 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+/*
+int Exponentiation (int number1, int number2)
+{
+    int exp = 1;
+    for (int i = 1; i <= number2; i++)
+        exp = exp * number1;
+    return exp;    
+}
+
+Console.Write("Input first integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = Exponentiation(a, b);
+Console.WriteLine($"The number {a} in {b} degree is {result}");
+*/
+
+//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+int SumOfDigit (int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number%10;
+        number = (number - number % 10) / 10;
+    }
+    return sum;
+}
+
+Console.Write("Input integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = SumOfDigit(a);
+Console.WriteLine($"Sum of the digits in the number {a} is {result}");
+*/
+
+//Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/*
+int [] CreateArray (int size)
+{
+    int [] newArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("$Input {i} elrment of the array: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+       Console.Write(array[i] + " ");
+
+     Console.WriteLine();  
+}
+
+Console.Write("inout size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray (a);
+ShowArray(myArray);
+*/
+
+
+
+
+
+
+
+
+
